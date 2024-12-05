3d6b57755d3caca3c22256be265696d0eb8ec7cb
Josh <889+Josh-A-T@users.noreply.github.com>
2024-12-04 23:27:37 -0600

    Update script.js

--- block07/script.js
+++ block07/script.js
@@ -1,4 +1,5 @@
-/*  */
+/*  Create 3 varibles called intA, intB, and intC and assign a variable based on an arhythmatic expression
+write to log using the string provided from fullstack and the combo*/
 let intA = 9-3;
 let intB = 3+5+1;
 let intC = (6-2)/2-1
