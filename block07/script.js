da17b7962648a154d2c7033406edf683df3110ea
Josh <889+Josh-A-T@users.noreply.github.com>
2024-12-04 23:11:28 -0600

    Update script.js

---
+++ block07/script.js
@@ -0,0 +1,5 @@
+/*  */
+let intA = 9-3;
+let intB = 3+5+1;
+let intC = (6-2)/2-1
+console.log(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`, intA, intB, intC);
